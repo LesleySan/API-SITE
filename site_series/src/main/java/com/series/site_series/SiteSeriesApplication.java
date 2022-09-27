7ed4c452704e66fb3eb02d9ba7682031e4840b4c
LesleySan <morenaroots16@gmail.com>
2022-09-27 20:11:07 -0300

    primeiro commit

---
+++ site_series/src/main/java/com/series/site_series/SiteSeriesApplication.java
@@ -0,0 +1,13 @@
+package com.series.site_series;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteSeriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteSeriesApplication.class, args);
+	}
+
+}
---
+++ site_series/src/test/java/com/series/site_series/SiteSeriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.series.site_series;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteSeriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
